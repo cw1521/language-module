5baa8685290e6c47c6f158afcbecba345244b30d
Christian Williams <cw1521@jagmail.southalabama.edu>
2023-07-12 01:01:05 -0500

    editied bin scripts

--- bin/run_en_st.sh
+++ bin/run_en_st.sh
@@ -1,1 +1,1 @@
-python3 ../language-module --task=en-st --model_checkpoint=cw1521/en-st-lg-40 --dataset_name=cw1521/en-st --model_name=en-st-lg-50 --num_epochs=10 --mode=train
+python3 ../language-module --task=en-st --model_checkpoint=cw1521/en-st-lg-40 --dataset_name=cw1521/nl-st-lg --model_name=en-st-lg-50 --num_epochs=10 --mode=train
--- bin/run_st_en.sh
+++ bin/run_st_en.sh
@@ -1,1 +1,1 @@
-python3 ../language-module --task=st-en --model_checkpoint=cw1521/st-en-lg-40 --dataset_name=cw1521/st-en --model_name=st-en-lg-50 --num_epochs=10 --mode=train
+python3 ../language-module --task=st-en --model_checkpoint=cw1521/st-en-lg-38 --dataset_name=cw1521/nl-st-lg --model_name=st-en-lg-40 --num_epochs=10 --mode=train
