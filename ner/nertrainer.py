de2f4230a59e37ed6fefdb8caa9f193036f39915
Christian Williams <cw1521@jagmail.southalabama.edu>
2023-07-12 00:46:49 -0500

    corrected batch_size for test runs (runs on RTX 3050 4GB laptop)

--- ner/nertrainer.py
+++ ner/nertrainer.py
@@ -94,9 +94,9 @@ class NERTrainer:
 
 
     def get_training_args(self, num_epochs):
-        batch_size = 256
+        batch_size = 64
         if self.test:
-            batch_size = 64
+            batch_size = 8
         args = TrainingArguments(
             self.model_name,
             save_steps=50,
