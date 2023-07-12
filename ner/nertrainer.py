6b30abb7f84cc46f49459b9875c0f093a5176974
Christian Williams <cw1521@jagmail.southalabama.edu>
2023-07-12 00:26:24 -0500

    ner_trainer.py imlemented and working

--- ner/nertrainer.py
+++ ner/nertrainer.py
@@ -59,9 +59,32 @@ class NERTrainer:
 
 
     def get_tokenized_datasets(self):
+        
+
         def tokenize_and_align_labels(examples):
-            tokenized_inputs = self.tokenizer(examples[self.input], truncation=True)
-            tokenized_inputs["labels"] = examples[self.target]
+            tokenized_inputs = self.tokenizer(examples[self.input], truncation=True, is_split_into_words=True)
+
+            labels = []
+            for i, label in enumerate(examples[self.target]):
+                word_ids = tokenized_inputs.word_ids(batch_index=i)
+                previous_word_idx = None
+                label_ids = []
+                for word_idx in word_ids:
+                    # Special tokens have a word id that is None. We set the label to -100 so they are automatically
+                    # ignored in the loss function.
+                    if word_idx is None:
+                        label_ids.append(-100)
+                    # We set the label for the first token of each word.
+                    elif word_idx != previous_word_idx:
+                        label_ids.append(label[word_idx])
+                    # For the other tokens in a word, we set the label to the current label
+                    else:
+                        label_ids.append(label[word_idx])
+                    previous_word_idx = word_idx
+
+                labels.append(label_ids)
+
+            tokenized_inputs["labels"] = labels
             return tokenized_inputs
 
         tokenized_data = self.dataset.map(tokenize_and_align_labels, batched=True)
