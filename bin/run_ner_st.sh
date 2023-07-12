b46951bcba0244a0267ddfd2b5e28efdb5a11d6a
Christian Williams <cw1521@jagmail.southalabama.edu>
2023-07-12 01:01:05 -0500

    editied bin scripts

--- bin/run_ner_st.sh
+++ bin/run_ner_st.sh
@@ -1,1 +1,1 @@
-python3 ../language-module --task=ner-st --model_checkpoint=cw1521/opus-mt-en-st --dataset_name=cw1521/en-st-ner-small --model_name=ner-st-sm-10 --num_epochs=10 --mode=train
+python3 ../language-module --task=ner-st --model_checkpoint=cw1521/opus-mt-en-st --dataset_name=cw1521/nl-st --model_name=ner-st-sm-10 --num_epochs=10 --mode=train
