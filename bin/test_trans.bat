7837cd40b3dcf7c4fecc961d085015653b954849
Christian Williams <cw1521@jagmail.southalabama.edu>
2023-07-12 05:10:49 -0500

    working

--- bin/test_trans.bat
+++ bin/test_trans.bat
@@ -1,1 +1,1 @@
-python ../language-module --task=en-st --model_checkpoint=cw1521/opus-mt-en-st --dataset_name=cw1521/nl-st --model_name=test-1 --num_epochs=1 --mode=test-train
+python ../language-module --task=en-st --model_checkpoint=cw1521/opus-mt-en-st --dataset_name=cw1521/nl-st --model_name=test-ner-1 --num_epochs=1 --mode=test-train
--- ner/nertrainer.py
+++ ner/nertrainer.py
@@ -88,6 +88,23 @@ class NERTrainer:
         return train, valid
 
 
+    def compute_metrics(self, p):
+        metric = load_metric("seqeval")
+        predictions, labels = p
+        predictions = np.argmax(predictions, axis=2)
+
+        true_predictions = [[self.label_list[p] for (p, l) in zip(prediction, label) if l != -100] for prediction, label in zip(predictions, labels)]
+        true_labels = [[self.label_list[l] for (p, l) in zip(prediction, label) if l != -100] for prediction, label in zip(predictions, labels)]
+
+        results = metric.compute(predictions=true_predictions, references=true_labels)
+        return {
+            "precision": results["overall_precision"], 
+            "recall": results["overall_recall"],
+            "f1": results["overall_f1"],
+            "accuracy": results["overall_accuracy"]
+        }
+
+
     def get_training_args(self, num_epochs):
         if self.test:
             batch_size = 8
@@ -122,23 +139,6 @@ class NERTrainer:
                 tf32=True
             )
         return args
-
-
-    def compute_metrics(self, p):
-        metric = load_metric("seqeval")
-        predictions, labels = p
-        predictions = np.argmax(predictions, axis=2)
-
-        true_predictions = [[self.label_list[p] for (p, l) in zip(prediction, label) if l != -100] for prediction, label in zip(predictions, labels)]
-        true_labels = [[self.label_list[l] for (p, l) in zip(prediction, label) if l != -100] for prediction, label in zip(predictions, labels)]
-
-        results = metric.compute(predictions=true_predictions, references=true_labels)
-        return {
-            "precision": results["overall_precision"], 
-            "recall": results["overall_recall"],
-            "f1": results["overall_f1"],
-            "accuracy": results["overall_accuracy"]
-        }
 
 
     def get_trainer(self, num_epochs):
