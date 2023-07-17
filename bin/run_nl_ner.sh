a502865c3f9ef581d1a53e811b643b5ab6524af8
Christian Williams <cw1521@jagmail.southalabama.edu>
2023-07-17 09:57:48 -0500

    changed batch_size in nertrainer.py

--- bin/run_nl_ner.sh
+++ bin/run_nl_ner.sh
@@ -1,1 +1,1 @@
-python3 ../language-module --task=nl-ner --model_checkpoint=distilbert-base-uncased --dataset_name=cw1521/nl-st --model_name=nl-ner-10 --num_epochs=10 --mode=train
+python3 ../language-module --task=nl-ner --model_checkpoint=bert-base-uncased --dataset_name=cw1521/nl-st --model_name=nl-ner-10 --num_epochs=10 --mode=train
