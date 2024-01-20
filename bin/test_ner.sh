5eaf4a25cd0dcb0583b3ba8d4d09dd9cd0b160c6
Christian Williams <cw1521@jagmail.southalabama.edu>
2024-01-19 20:26:07 -0600

    changed seqeval to use evaluate(HF)

--- bin/test_ner.sh
+++ bin/test_ner.sh
@@ -1,1 +1,1 @@
-python3 ../language-module --task=nl-ner --model_checkpoint=distilbert-base-uncased --dataset_name=cw1521/en-st-ner --model_name=nl-ner-10 --num_epochs=1 --mode=test --mode=train
+python3 ../language-module --task=nl-ner --model_checkpoint=distilbert-base-uncased --dataset_name=cw1521/nl-st --model_name=nl-ner-10 --num_epochs=1 --mode=test
