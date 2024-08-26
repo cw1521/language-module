031510ae93c717678b4843dbb8ab2efcffff4b8f
Christian Williams <cw1521@jagmail.southalabama.edu>
2024-08-26 11:53:12 -0500

    changed readme and added absolute path to .bat files

--- bin/test_trans.bat
+++ bin/test_trans.bat
@@ -1,1 +1,1 @@
-python ../language-module --task=en-st --model_checkpoint=cw1521/opus-mt-en-st --dataset_name=cw1521/nl-st --model_name=test-ner-1 --num_epochs=1 --mode=test-train
+python C:\Users\school\thesis\code\language-module --task=en-st --model_checkpoint=cw1521/opus-mt-en-st --dataset_name=cw1521/nl-st --model_name=test-ner-1 --num_epochs=1 --mode=test-train
