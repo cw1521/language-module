4d47a25eb769d976a401561fa357287a97afce6f
Christian Williams <cw1521@jagmail.southalabama.edu>
2024-08-26 11:54:12 -0500

    changed readme and removed absolute path to .bat files

--- bin/test_ner.bat
+++ bin/test_ner.bat
@@ -1,1 +1,1 @@
-python C:\Users\school\thesis\code\language-module --task=nl-ner --model_checkpoint=distilbert-base-uncased --dataset_name=cw1521/nl-st --model_name=nl-ner-1 --num_epochs=1 --mode=test-train
+python ../language-module --task=nl-ner --model_checkpoint=distilbert-base-uncased --dataset_name=cw1521/nl-st --model_name=nl-ner-1 --num_epochs=1 --mode=test-train
