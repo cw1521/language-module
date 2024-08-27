6c6e6f4d9141d7b294392427f3ff8b84c2c65905
Christian Williams <cw1521@jagmail.southalabama.edu>
2024-08-26 22:10:28 -0500

    changed bash and bat files to run as module

--- bin/run_ner_st.sh
+++ bin/run_ner_st.sh
@@ -1,1 +1,2 @@
-python3 ../language-module --task=ner-st --model_checkpoint=cw1521/opus-mt-en-st --dataset_name=cw1521/nl-st --model_name=ner-st-10 --num_epochs=10 --mode=train
+cd ..
+python3 -m language-module --task=ner-st --model_checkpoint=cw1521/opus-mt-en-st --dataset_name=cw1521/nl-st --model_name=ner-st-10 --num_epochs=10 --mode=train
--- bin/test_trans.bat
+++ bin/test_trans.bat
@@ -1,1 +1,2 @@
-python ../language-module --task=en-st --model_checkpoint=cw1521/opus-mt-en-st --dataset_name=cw1521/nl-st --model_name=test-ner-1 --num_epochs=1 --mode=test-train
+cd ..
+python -m language-module --task=en-st --model_checkpoint=cw1521/opus-mt-en-st --dataset_name=cw1521/nl-st --model_name=test-trans-1 --num_epochs=1 --mode=test
