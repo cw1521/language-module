3a394895a45b8c14600b55a571f27aea2b6fe0a2
Christian Williams <cw1521@jagmail.southalabama.edu>
2024-09-04 10:27:54 -0500

    changed scripts

--- bin/run_ner_st.sh
+++ bin/run_ner_st.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-curr_dir="$(pwd)"
-script_dir="$(dirname "$(readlink -f "$0")")"
-cd $script_dir/../..
+# curr_dir="$(pwd)"
+# script_dir="$(dirname "$(readlink -f "$0")")"
+# cd $script_dir/../..
 
 python3 -m language-module --task=ner-st --model_checkpoint=cw1521/opus-mt-en-st --dataset_name=cw1521/nl-st --model_name=ner-st-10 --num_epochs=10 --mode=train
 
-cd $script_dir
+# cd $script_dir
