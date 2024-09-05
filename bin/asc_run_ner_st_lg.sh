fd47b40f0c59bb2a18bb630209139a2dce1d200e
Christian Williams <cw1521@jagmail.southalabama.edu>
2024-09-04 21:07:04 -0500

    added ner_st scripts

---
+++ bin/asc_run_ner_st_lg.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+curr_dir="$(pwd)"
+script_dir="$(dirname "$(readlink -f "$0")")"
+cd $script_dir/../..
+
+
+
+singularity exec --nv docker://huggingface/transformers-pytorch-gpu python3 -m language-module --task=ner-st --model_checkpoint=cw1521/opus-mt-en-st --dataset_name=cw1521/nl-st-lg --model_name=ner-st-lg-10 --num_epochs=10 --mode=train
+
+
+cd $script_dir
