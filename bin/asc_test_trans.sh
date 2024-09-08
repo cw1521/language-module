cf35ac0aa398e32998b69721365ae5149a4692c2
kolitha warnakulasooriya <hkw2021@jagmail.southalabama.edu>
2024-09-08 18:48:15 -0500

    changed translationtrainer to eval

--- bin/asc_test_trans.sh
+++ bin/asc_test_trans.sh
@@ -5,7 +5,7 @@
 # cd $script_dir/../..
 cd ../../
 
-apptainer exec --nv ./hf-apptainer/hf.sif python3 -m language-module --task=ner-st --model_checkpoint=Helsinki-NLP/opus-mt-en-fr --dataset_name=cw1521/nl-st --model_name=ner-st-1 --num_epochs=1 --mode=train --batch_size=512
+apptainer exec --nv ./hf-apptainer/hf.sif python3 -m language-module --task=ner-st --model_checkpoint=Helsinki-NLP/opus-mt-en-fr --dataset_name=cw1521/nl-st --model_name=ner-st-1 --num_epochs=1 --mode=test --batch_size=512
 
 
 # cd $curr_dir
