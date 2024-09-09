4b035a9b1ddd7a2f3a0a56a2c039a985d14f0ee2
kolitha warnakulasooriya <hkw2021@jagmail.southalabama.edu>
2024-09-09 03:02:03 -0500

    nertrainer working

--- bin/asc_test_nl_ner.sh
+++ bin/asc_test_nl_ner.sh
@@ -6,6 +6,6 @@ cd $script_dir/../..
 
 
 # apptainer exec --nv ./hf-apptainer/hf.sif
-python3 -m language-module --task=nl-ner --model_checkpoint=distilbert-base-uncased --dataset_name=cw1521/nl-st --model_name=test-ner-1 --num_epochs=1 --mode=test --batch_size=32
+python3 -m language-module --task=nl-ner --model_checkpoint=bert-base-cased --dataset_name=cw1521/nl-st --model_name=test-ner-1 --num_epochs=1 --mode=test --batch_size=512
 
 cd $curr_dir
