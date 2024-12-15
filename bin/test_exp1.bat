001f5c49ccbf71c92b1eae9c130655b1bbcb0946
Christian Williams <cw1521@jagmail.southalabama.edu>
2024-12-14 22:21:50 -0600

    added experiment 1 (tested)

---
+++ bin/test_exp1.bat
@@ -0,0 +1,7 @@
+SET script_path=%~dp0
+SET curr_path=%CD%
+cd %script_path%..\..
+
+python -m language-module --mode=exp --exp=exp1 --s1=cw1521/st-en-50 --r1=cw1521/en-st-50 --dataset_name=cw1521/nl-st --output=st-nl-st-50.jsonl
+
+cd %curr_path%
---
+++ experiments/__init__.py
@@ -0,0 +1,3 @@
+__all__=["perform_experiment1"]
+
+from .experiment1 import perform_experiment1
