9393da9f1ceca41ae78ce4e8056f5554d2ee30f0
Christian Williams <cw1521@jagmail.southalabama.edu>
2025-03-15 20:52:15 -0500

    changed exp1 to a pipeline

--- experiments/experiment1.py
+++ experiments/experiment1.py
@@ -1,10 +1,14 @@
-from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM, pipeline
 import sacremoses
 from time import time
 from os import getcwd
 from ..langhelper import print_log, print_result, create_folder_if_not_exists
 from ..langhelper import write_log, get_dataset, json_to_file
 
+
+def data(ds):
+    for data in ds:
+        yield data
 
 
 def process_dataset(ds, sender_checkpoint, receiver_checkpoint, hf_token, output_folder, output_file):
@@ -16,35 +20,35 @@ def process_dataset(ds, sender_checkpoint, receiver_checkpoint, hf_token, output
     receiver_model = AutoModelForSeq2SeqLM.from_pretrained(receiver_checkpoint, token=hf_token)
     receiver_tokenizer = AutoTokenizer.from_pretrained(receiver_checkpoint, token=hf_token)
 
+    sender_transcriber = pipeline(task="translation", model=sender_model, tokenizer=sender_tokenizer, device=0)
+    receiver_transcriber = pipeline(task="translation", model=receiver_model, tokenizer=receiver_tokenizer, device=0)
+
+    ds_it=data(ds)
+
+    results_list=[]
 
     count=0
     ds_size=len(ds)
-    results_list=[]
-    for text in ds:    
-        # print(text)
-        sender_translated=sender_model.generate(**sender_tokenizer(text, return_tensors="pt", padding=True))
-        # print(sender_translated)
-        sender_decoded=sender_tokenizer.decode(sender_translated[0], skip_special_tokens=True)
-        # print(sender_decoded)
-        receiver_translated=receiver_model.generate(**receiver_tokenizer(sender_decoded, return_tensors="pt", padding=True))
-        receiver_decoded=receiver_tokenizer.decode(receiver_translated[0], skip_special_tokens=True)
+
+    for sender_out in sender_transcriber(ds_it):
+        sender_message=sender_out[0]["translation_text"]
+        # print(sender_message)
+        receiver_message=receiver_transcriber(sender_message)[0]["translation_text"]
 
         result={}
-        result["target"]=text
-        result["nl"]=sender_decoded
-        result["predicted"]=receiver_decoded
+        result["target"]=sender_message
+
+        result["predicted"]=receiver_message
 
         results_list.append(result)
 
         count+=1
 
-        print_log(count, START_TIME, ds_size)
-
-        if count % 100 == 0 or count == ds_size:
-            write_log(count, len(ds), START_TIME, output_folder)
         if count % 500 == 0 or count == ds_size:
             json_to_file(results_list, output_folder, output_file)
             results_list=[]
+
+
 
 
 
