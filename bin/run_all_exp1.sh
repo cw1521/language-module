6d90bc31a2e61260718f9684551253269cae3cb7
Christian Williams <cw1521@jagmail.southalabama.edu>
2025-04-29 21:48:45 -0500

    run_all_exp scripts added

---
+++ bin/run_all_exp1.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+cd $1
+
+scripts=(
+    "./language-module/bin/run_exp.sh 1 10"
+    "./language-module/bin/run_exp.sh 1 20"
+    "./language-module/bin/run_exp.sh 1 30"
+    "./language-module/bin/run_exp.sh 1 40"
+    "./language-module/bin/run_exp.sh 1 50"
+)
+
+for script in "${scripts[@]}"; do
+    bash "$script" & disown
+done 
+
