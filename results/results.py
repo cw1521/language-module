61be9f0d1e858f02a47d352c7ab98475eb640895
Christian Williams <cw1521@jagmail.southalabama.edu>
2025-05-19 16:56:35 -0500

    edited results.py for large experiment 1

--- results/results.py
+++ results/results.py
@@ -61,7 +61,7 @@ def get_results(files, exp_type):
     results=init_results()
     avg_results=init_avg_results(files)
     for i in files:
-        fname=f"./language-module/data/results-{exp_type}-{i}.jsonl"
+        fname=f"./language-module/data/results/{exp_type}-{i}-eval/{exp_type}-{i}-eval.jsonl"
         ds=load_ds(fname)
         stats=get_stats(ds)
         results[i]=stats
@@ -100,57 +100,78 @@ def show_table(df, title):
 
 
 
-def show_line_graph(field, ds, x_values, exp):
+def show_line_graph(field, ds, x_values, title):
     y_values=get_y_values(field, ds, x_values)
     if field=="dloss":
         field="Domain Loss"
     display_line_graph(x_values, y_values, "epoch", field, 
-                       f"{field} from Experiment {exp}")
+                       f"{field} from {title}")
 
 
 
 def main():
     st_nl_files=["10", "20", "30", "40", "50"]
-    nl_ner_st_files=["10", "20","30"]
+    # nl_ner_st_files=["10", "20","30"]
 
     st_nl_avg=get_results(st_nl_files, "st-nl-st")
-    nl_ner_avg=get_results(nl_ner_st_files, "nl-ner-st")
+    st_nl_lg_avg=get_results(st_nl_files, "st-nl-st-lg")
+    # nl_ner_avg=get_results(nl_ner_st_files, "nl-ner-st")
 
     st_nl_df=pd.DataFrame(st_nl_avg).round(3)
-    nl_ner_df=pd.DataFrame(nl_ner_avg).round(3)
+    st_nl_lg_df=pd.DataFrame(st_nl_lg_avg).round(3)
+    # nl_ner_df=pd.DataFrame(nl_ner_avg).round(3)
 
+    exp1_title="Experiment 1 (Dataset 1)"
+    exp1_lg_title="Experiment 1 (Dataset 2)"
 
-    show_table(st_nl_df, "Experiment 1")
-    show_table(nl_ner_df, "Experiment 2")
+    show_table(st_nl_df, exp1_title)
+    show_table(st_nl_lg_df, exp1_lg_title)
+    # show_table(nl_ner_df, "Experiment 2")
 
 
 
     # Avg Loss
-    show_line_graph("loss", st_nl_avg, st_nl_files, "1")
+    show_line_graph("loss", st_nl_avg, st_nl_files, exp1_title)
 
     # Avg Domain Loss
-    show_line_graph("dloss", st_nl_avg, st_nl_files, "1")
+    show_line_graph("dloss", st_nl_avg, st_nl_files, exp1_title)
 
     # Jaccard
-    show_line_graph("jaccard", st_nl_avg, st_nl_files, "1")
+    show_line_graph("jaccard", st_nl_avg, st_nl_files, exp1_title)
 
     # Cosine
-    show_line_graph("cosine", st_nl_avg, st_nl_files, "1")
+    show_line_graph("cosine", st_nl_avg, st_nl_files, exp1_title)
 
 
+    # Experiment 1 (Dataset 2)
 
-    ### NL-NER-ST
+
     # Avg Loss
-    show_line_graph("loss", nl_ner_avg, nl_ner_st_files, "2")
+    show_line_graph("loss", st_nl_lg_avg, st_nl_files, exp1_lg_title)
 
     # Avg Domain Loss
-    show_line_graph("dloss", nl_ner_avg, nl_ner_st_files, "2")
+    show_line_graph("dloss", st_nl_lg_avg, st_nl_files, exp1_lg_title)
 
     # Jaccard
-    show_line_graph("jaccard", nl_ner_avg, nl_ner_st_files, "2")
+    show_line_graph("jaccard", st_nl_lg_avg, st_nl_files, exp1_lg_title)
 
     # Cosine
-    show_line_graph("cosine", nl_ner_avg, nl_ner_st_files, "2")
+    show_line_graph("cosine", st_nl_lg_avg, st_nl_files, exp1_lg_title)
+
+
+
+    # ### NL-NER-ST
+    # # Avg Loss
+    # show_line_graph("loss", nl_ner_avg, nl_ner_st_files, "2")
+
+    # # Avg Domain Loss
+    # show_line_graph("dloss", nl_ner_avg, nl_ner_st_files, "2")
+
+    # # Jaccard
+    # show_line_graph("jaccard", nl_ner_avg, nl_ner_st_files, "2")
+
+    # # Cosine
+    # show_line_graph("cosine", nl_ner_avg, nl_ner_st_files, "2")
 
 
 
